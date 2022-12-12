90a2902a6098038b6447ca6553841509d1a49396
MDog37127 <bentleymdog@gmail.com>
2022-12-12 17:41:04 -0600

    Initial Commit

---
+++ Swift_Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Notes
+//
+//  Created by McLennan Jones on 12/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
