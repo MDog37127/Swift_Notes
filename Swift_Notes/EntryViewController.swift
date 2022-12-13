4eff6ad50e707ea4c17e776bae55fa62329fbc83
MDog37127 <bentleymdog@gmail.com>
2022-12-12 19:31:42 -0600

    added all the thing to make it work'

---
+++ Swift_Notes/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Swift_Notes
+//
+//  Created by McLennan Jones on 12/12/22.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+    
+    public var completion: ((String, String) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+        // Do any additional setup after loading the view.
+    }
+    @objc func didTapSave() {
+        if let text = titleField.text,  !text.isEmpty, !noteField.text.isEmpty {
+            completion?(text, noteField.text)
+        }
+    }
+
+}
---
+++ Swift_Notes/NoteViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NoteViewController.swift
+//  Swift_Notes
+//
+//  Created by McLennan Jones on 12/12/22.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+
+}
